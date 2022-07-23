8069086941907f5741916c9f90fa4787585677e5
Lalo <lalo.valadezvv1@gmail.com>
2022-07-23 02:31:19 -0500

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
 
 let tabHeader = document.getElementsByClassName("tab-header")[0];
 let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
@@ -20,6 +21,76 @@ tabsPane[i].addEventListener("click", function(){
 }
 
 menu.onclick = () => {
+    navbar.classList.toggle('open-menu');
     menu.classList.toggle('move');
 }
 
+window.onscroll = () => {
+    navbar.classList.remove('open-menu');
+    menu.classList.remove('move');
+}
+
+//Email Js
+
+function validate(){
+    let name = document.querySelector('.name');
+    let email = document.querySelector('.email');
+    let message = document.querySelector('.message')
+
+    let sendBtn = document.querySelector('.send-btn');
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (email.value == "" || name.value == "" || message.value == ""){
+            emptyerror()
+        } else {
+            sendemail(name.value, email.value, message.value);
+            success();
+            name.value = "";
+            email.value = "";
+            message.value = "";
+        }
+    });
+
+}
+
+validate();
+
+function sendemail(name,email,message){
+    emailjs.send("service_naeax8p","template_9oupyud",{
+        to_name: name,
+        from_name: email,
+        message: message,
+        });
+}
+
+function emptyerror(){
+    swal({
+        title: "Oh no...",
+        text: "Fields cannot be empty!",
+        icon: "error",
+      });
+}
+
+function success(){
+    swal({
+        title: "Email send successfully",
+        text: "We try to reply in 24 hours",
+        icon: "success",
+      });
+}
+
+// Header background change on scroll
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('header-active', window.scrollY > 0);
+});
+
+// Scroll top
+
+let scrollTop = document.querySelector('.scroll-top');
+
+window.addEventListener('scroll', () => {
+    scrollTop.classList.toggle('scroll-active', window.scrollY > 400);
+});
